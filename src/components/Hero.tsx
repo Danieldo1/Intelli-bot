6b3cc73c7b5ab6f8804c5cc37df559a43583744c
Dan <daniel.speranskiy@live.com>
2023-12-12 16:27:09 +0400

    Connecting refs now

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,18 +11,18 @@ const Hero = () => {
     className="pt-32 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-48 md:px-12 px-4 bg-secondary">
     <div className="md:flex-1 md:mr-10 container">
       <h1 className="font-pt-serif text-5xl font-bold mb-7 flex-1">
-      Your Gateway to Intelligent Conversations, <br />
+      Your Gateway to Intelligent Conversations. <br />
         <span className="pb-2 text-blue-500">
           Powered by Web Knowledge and ChatGPT-4 Brilliance.
         </span>
       </h1>
       <p className="font-pt-serif font-normal mb-7">
-      Unlock the Power of Intelligent Conversations with IntelliChat: Seamlessly Bridging the Web's Vast Knowledge and the Brilliance of ChatGPT-4, Redefining the Way You Engage and Interact Online.
+      Unleash the potential of smart dialogue through IntelliChat. It's where the immense knowledge of the web meets the ingenuity of ChatGPT-4, taking your online communication and interaction to new heights of excitement and engagement.
       </p>
       <div className="font-montserrat justify-center items-center flex-row flex">
-        <button className="bg-black px-6 py-4 rounded-lg border-2 border-black border-solid text-white mr-2 mb-2 ">
-          <Link className='' href='https://ko-fi.com/R6R6S3XAX' target='_blank'> 
-          Support free project
+        <button className="bg-black px-6 py-4 rounded-lg border-2 border-black border-solid text-white mr-2 mb-2 hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[14px_11px_0px_1px_rgba(0,0,0,0.75)]">
+          <Link className='font-semibold' href='https://ko-fi.com/R6R6S3XAX' target='_blank'> 
+          Support this project
           <div className=' justify-center items-center flex'>
             <Image height='36' width='50' src='https://storage.ko-fi.com/cdn/brandasset/kofi_s_logo_nolabel.png?_gl=1*trhf8t*_ga*Mzk1NDY5MDkyLjE3MDIzNzAzNDY.*_ga_M13FZ7VQ2C*MTcwMjM3MDM0NS4xLjEuMTcwMjM3MjI4Ni40Mi4wLjA.'  alt='Buy Me a Coffee at ko-fi.com' />
           </div>
